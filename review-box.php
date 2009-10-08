146ed2f236164a741864c6d7a4c637c7b3aee12c
paradox460 <paradox460@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-08 00:40:35 +0000

    Version 1.5
    Css changes
    * Removed css percentage based calculation, now done with php and pixel values
    * Added text shadows
    PHP Changes
    * return now uses a variable, instead of a big long block of text
    * Bar width defined in PHP, instead of css, and is calculated in pixel values
    * Added new shortcode params:
      * title
      * verdict
    
    
    
    git-svn-id: https://plugins.svn.wordpress.org/review-box/trunk@161440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- review-box.php
+++ review-box.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Review Box
-Version: 1.0.3
+Version: 1.5
 Plugin URI: http://www.paradoxdgn.com/archives/622
-Description: Creates a box for reviewers, with pros, cons, and a score bar. Use <code>[review pros="pros" cons="cons" score=85]</code> to insert. <em>score must be a percentage value, without the % sign</em>
+Description: Creates a box for reviewers, with pros, cons, and a score bar. Use <code>[review pros="pros" cons="cons"  score=85]</code> to insert. <em>score must be a percentage value, without the % sign</em>
 Author: Paradox
 Author URI: http://paradoxdgn.com
 */
@@ -29,12 +29,14 @@ function addCSS() {
 	echo '<link type="text/css" rel="stylesheet" href="' . plugins_url ( plugin_basename ( dirname ( __FILE__ ) ) ) .'/review-box.css" />';
 
 }
-
 function review_prettify($atts) {
 	extract( shortcode_atts( array(
+		'title' => 'Review',
+		'title_url' => '',
 		'score' => '100',
 		'pros' => '<em>None</em>',
 		'cons' => '<em>None</em>',
+		'verdict' => '',
 		), $atts ));
 	if ( $score > 100 ) {
 		$score = 100;
@@ -42,20 +44,39 @@ function review_prettify($atts) {
 	elseif ( $score < 0 ) {
 		$score = 0;
 	}
-	return '<a name="review"></a><div class="review">
-		<h2>Review</h2>
+	$width = 450;
+	/*
+	The width of the review box is controlled programattically this time, instead of via CSS. This should produce a better result. If you want to change this value, set it above. THIS MUST BE IN PIXEL (PX) VALUES.
+
+In newer versions, this should be an admin page option
+*/
+	#title system.
+	if ( $title == 'none' ) {
+		$title_bar = '';
+	} else {
+		$title_bar = '<h2>'. do_shortcode($title) .'</h2>';
+	}
+	#Verdict box
+	if ( $verdict == null ) {
+		$verdict_box = '';
+	} else {
+		$verdict_box = '<tr><th>Verdict</th><td>'. $verdict .'</td></tr>';
+	}
+	$result = '<a name="review"></a><div class="review">
+		'. $title_bar .'
 		<div class="mainbox">
 			<div class="procons">
 	<table>
 			<tr><th>Pros</th><th>Cons</th></tr>
 			<tr><td>' . do_shortcode($pros) . '</td><td>' . do_shortcode($cons) . '</td></tr>
+			' . $verdict_box . '
 			</table>
 			</div>
                         <table class="review_grid">
-                                <tr><td class="review_label">Rating</td><td><div class="rating_bg"><span class="rating_bar" style="width: ' . $score . '%;"><span class="rating_bar_content">' . $score . '%</span></span></div></td></tr>
+                                <tr><td class="review_label">Rating</td><td><div class="rating_bg" style="width: ' . $width . 'px"><span class="rating_bar" style="width: ' . $width * ($score/100) . 'px;"><span class="rating_bar_content">' . $score . '%</span></span></div></td></tr>
                         </table>
 		</div></div>';
-
+	return $result;
 
 }
 
